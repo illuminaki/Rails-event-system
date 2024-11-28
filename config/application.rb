b9b9a0d61ce250945a2a8d8c0927b2977306c597
Sebastian Agudelo <jsagudeloaa@gmail.com>
2024-11-28 08:09:24 -0500

    Translation confi with routes

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module EventSystem
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.active_job.queue_adapter = :sidekiq
+    # Set default locale to Spanish
+    config.i18n.default_locale = :es
+    config.i18n.available_locales = [:en, :es, :ja]
+    # Ensure all translations from config/locales/*.rb,yml are auto loaded
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 
   end
 end
